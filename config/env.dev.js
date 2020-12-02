108de656ce179d3ef0e4ae14c240c249904dcd4c
Jeff Soriano <soriano.jeffm@gmail.com>
2020-12-02 13:09:31 -0800

    Update env.dev.js with AWS variables

--- config/env.dev.js
+++ config/env.dev.js
@@ -2,10 +2,24 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const clientOriginUrl = process.env.CLIENT_ORIGIN_URL;
+
+/* AUTH0 */
 const audience = process.env.AUTH0_AUDIENCE;
 const domain = process.env.AUTH0_DOMAIN;
-const serverPort = process.env.SERVER_PORT;
-const clientOriginUrl = process.env.CLIENT_ORIGIN_URL;
+
+/* AWS */
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region = process.env.AWS_REGION;
+const bucketName = process.env.AWS_BUCKET_NAME;
+
+
+if (!clientOriginUrl) {
+    throw new Error(
+        ".env is missing the definition of a CLIENT_ORIGIN_URL environmental variable",
+    );
+}
 
 if (!audience) {
     throw new Error(
@@ -19,21 +33,36 @@ if (!domain) {
     );
 }
 
-if (!serverPort) {
+if (!accessKeyId) {
     throw new Error(
-        ".env is missing the definition of a API_PORT environmental variable",
+        ".env is missing the definition of an AWS_ACCESS_KEY_ID environmental variable",
     );
 }
 
-if (!clientOriginUrl) {
+if (!secretAccessKey) {
     throw new Error(
-        ".env is missing the definition of a APP_ORIGIN environmental variable",
+        ".env is missing the definition of an AWS_SECRET_ACCESS_KEY environmental variable",
+    );
+}
+
+if (!region) {
+    throw new Error(
+        ".env is missing the definition of an AWS_REGION environmental variable",
+    );
+}
+
+if (!bucketName) {
+    throw new Error(
+        ".env is missing the definition of an AWS_BUCKET_NAME environmental variable",
     );
 }
 
 module.exports = {
+    clientOriginUrl,
     audience,
     domain,
-    serverPort,
-    clientOriginUrl
+    accessKeyId,
+    secretAccessKey,
+    region,
+    bucketName
 };
